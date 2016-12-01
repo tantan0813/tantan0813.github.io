aec554708883403eaf5fa3aade9d190a3ef31b20
tantan0813 <1418346401@qq.com>
2016-12-01 17:39:36 +0800

    1

--- js/es/index.js
+++ js/es/index.js
@@ -4,13 +4,22 @@
 window.onload = function(){
     change_main_bg();
     function change_main_bg(){
+        //取随机数
+        function selectfrom (lowValue,highValue){
+            var choice=highValue-lowValue+1;
+            return Math.floor(Math.random()*choice+lowValue);
+        };
         setInterval(function(){
           var bg = document.getElementsByClassName("top")[0],
-              text = document.getElementsByClassName("text"),
-              index = Math.round(Math.random()*9+1);
-            bg.style.background="url(img/bg-main-"+index+".jpg)";
-            text[index].style.display="block"?"none":"block"
-
-        },3000);
+              bgIndex = selectfrom (1,10);
+            bg.style.background="url(img/bg-main-"+bgIndex+".jpg)";
+            text[textIndex].style.display=="none" ? text[textIndex].style.display="block" :text[textIndex].style.display="none";
+        },4000);
+        setInterval(function (){
+            var text = document.getElementsByClassName("text"),
+                textIndex = selectfrom (0,3);
+            // text[textIndex].style.display=="none" && text[textIndex].style.display="block" ;
+            text[textIndex].style.display=="none" ? text[textIndex].style.display="block" :text[textIndex].style.display="none";
+        },2000)
     }
 }
