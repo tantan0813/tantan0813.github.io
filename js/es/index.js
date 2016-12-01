c30cbe16a724f1c67308219289d8d0390db2b4c3
tantan0813 <1418346401@qq.com>
2016-12-01 19:18:41 +0800

    2

--- js/es/index.js
+++ js/es/index.js
@@ -15,13 +15,13 @@ window.onload = function(){
               bgIndex = selectfrom (1,10);
             bg.style.background="url(img/bg-main-"+bgIndex+".jpg)";
             text[textIndex].style.display=="none" ? text[textIndex].style.display="block" :text[textIndex].style.display="none";
-        },4000);
+        },6000);
         //文字框定时器
         setInterval(function (){
             var text = document.getElementsByClassName("text"),
                 textIndex = selectfrom (0,3);
             // text[textIndex].style.display=="none" && text[textIndex].style.display="block" ;
             text[textIndex].style.display=="none" ? text[textIndex].style.display="block" :text[textIndex].style.display="none";
-        },2000);
+        },8000);
     }
 }
