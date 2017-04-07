aca5dd6c3737e8c97943ce4599ccb2eb70254940
tantan0813 <1418346401@qq.com>
2017-04-07 09:25:54 +0800

    232323

--- js/es/paging.js
+++ js/es/paging.js
@@ -13,7 +13,7 @@ $(function () {
   var list = Array.prototype.splice.call(_list, 0); //转化为数组
   // console.log(_list,typeof _list,typeof list,_list.length,list.length);
   // console.log(list[2],1101010);
-  var show_bg = list.slice(0, 5);
+  var show_bg = _list.slice(0, 5);
   var show_no = list.slice(5);
   console.log(show_bg, show_no);
   show_bg.setAttribute("display", "block");
