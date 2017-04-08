2a36b84a6ced5a730e3b75072537e0a48f1534d9
tantan0813 <1418346401@qq.com>
2017-04-08 17:25:03 +0800

    000000

--- js/es/paging.js
+++ js/es/paging.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 /**
  * Created by tanmi on 2016/12/7.
  */
@@ -11,12 +9,26 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 
 $(function () {
-    // var list=$(".list");
-    var list = document.getElementsByClassName("list");
-    // var show_bg=list.slice(0,5);
-    // var show_no=list.slice(5);
-    console.log(list, typeof list === "undefined" ? "undefined" : _typeof(list), list.length, list[0]);
-    show_bg.setAttribute("display", "block");
-    show_no.setAttribute("display", "none");
-    var pages = Math.ceil(list.length / 5);
+    page($("#skill>div"), 2, $("#skill"), "page_active");
+    function page(content, page_len, place, active) {
+        //参数依次为分页内容体、每页的内容数量，页码的添加位置，页码活跃时的样式
+        var list = content;
+        var pages = Math.ceil(list.length / page_len);
+        var s = "<ul class='page_index'><li class='pre' title='当前前五页'>pre</li>";
+        for (var i = 1; i <= pages; i++) {
+            s += "<li class='page_num'>" + i + "</li>";
+        }
+        s += "<li class='next' title='当前后五页'>next</li></ul>";
+        place.after(s);
+        $(".page_num").on("click", function () {
+            var num = $(this).text() * page_len;
+            $(this).addClass(active).siblings().removeClass(active);
+            var show_ing = list.slice(num - page_len, num);
+            var show_no_next = list.slice(num);
+            var show_no_pre = list.slice(0, num - page_len);
+            show_ing.css("display", "block");
+            show_no_pre.css("display", "none");
+            show_no_next.css("display", "none");
+        });
+    }
 });
--- js/es6/paging.js
+++ js/es6/paging.js
@@ -7,14 +7,27 @@
 
 
 $(function() {
-    // var list=$(".list");
-    var list = document.getElementsByClassName("list");
-    // var show_bg=list.slice(0,5);
-    // var show_no=list.slice(5);
-    console.log(list,typeof list,list.length,list[0]);
-    show_bg.setAttribute("display", "block");
-    show_no.setAttribute("display", "none");
-    var pages=Math.ceil(list.length/5);
+    page($("#skill>div"),2,$("#skill"),"page_active");
+    function page(content,page_len,place,active){//参数依次为分页内容体、每页的内容数量，页码的添加位置，页码活跃时的样式
+        var list = content;
+        var pages=Math.ceil(list.length/page_len);
+        var s="<ul class='page_index'><li class='pre' title='当前前五页'>pre</li>";
+        for(var i=1;i<=pages;i++){
+            s+="<li class='page_num'>"+i+"</li>";
+        }
+        s+="<li class='next' title='当前后五页'>next</li></ul>";
+        place.after(s);
+        $(".page_num").on("click",function(){
+            var num = $(this).text()*page_len;
+            $(this).addClass(active).siblings().removeClass(active);
+            var show_ing=list.slice(num-page_len,num);
+            var show_no_next=list.slice(num);
+            var show_no_pre=list.slice(0,num-page_len);
+            show_ing.css("display", "block");
+            show_no_pre.css("display", "none");
+            show_no_next.css("display", "none");
+        });
+    }
 });
 
 
