14dfe7563e6c15183f5c8973ad1d4c4733fd8330
tantan0813 <1418346401@qq.com>
2017-04-14 17:24:13 +0800

    test

--- js/es/paging.js
+++ js/es/paging.js
@@ -13,7 +13,6 @@ $(function () {
     var rip = document.getElementsByClassName("aside");
     ripple(rip);
     function ripple(target) {
-        console.log(30);
         var canvas = {},
             centerX = 0,
             centerY = 0,
@@ -23,7 +22,7 @@ $(function () {
             element = {},
             radius = 0,
 
-        // 根据callback生成requestAnimationFrame动画
+        // 根据callback生成requestAnimationFrame动画,requestAnimationFrame 请求动画帧原理setTimeout
         requestAnimFrame = function () {
             return window.requestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
                 window.setTimeout(callback, 1000 / 60);
@@ -33,24 +32,20 @@ $(function () {
         // 为每个指定元素生成canves
         init = function init() {
             containers = Array.prototype.slice.call(containers);
-            // console.log(containers.length);
             for (var i = 0; i < containers.length; i += 1) {
                 canvas = document.createElement('canvas');
                 canvas.addEventListener('click', press, false);
                 containers[i].appendChild(canvas);
                 canvas.style.width = '100%';
                 canvas.style.height = '100%';
-                canvas.style.opacity = '0.1';
+                canvas.style.opacity = '0.16';
                 canvas.width = canvas.offsetWidth;
                 canvas.height = canvas.offsetHeight;
-                console.log(canvas);
             }
         },
 
         // 点击并且获取需要的数据，如点击坐标、元素大小、颜色
         press = function press(event) {
-            console.log(event);
-            color = '#72E1E6';
             element = event.toElement;
             context = element.getContext('2d');
             radius = 0;
@@ -64,19 +59,26 @@ $(function () {
         draw = function draw() {
             context.beginPath();
             context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-            context.fillStyle = color;
+            context.fillStyle = colorS();
             context.fill();
             radius += 2;
             // 通过判断半径小于元素宽度，不断绘制 radius += 2 的圆形
-            if (radius < element.height) {
+            if (radius < 40) {
                 requestAnimFrame(draw);
             }
         };
-        //随机取颜色值
+        //随机取颜色值rgb色值是十进制，10--16的进制转换
         colorS = function colorS() {
-            var num = function num(star, end) {
-                var n = Math.r;
-            };
+            var s = "#";
+            function num(star, end) {
+                //随机取整
+                var n = Math.random() * end + star;
+                return parseInt(n);
+            }
+            s += num(0, 255).toString(16);
+            s += num(0, 255).toString(16);
+            s += num(0, 255).toString(16);
+            return s;
         };
         init();
     }
