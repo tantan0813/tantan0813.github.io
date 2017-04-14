abcc9bcae782e3d34dbe643a23db5f7acf85a3fb
tantan0813 <1418346401@qq.com>
2017-04-14 18:24:48 +0800

    test

--- js/es/paging.js
+++ js/es/paging.js
@@ -13,7 +13,7 @@ $(function () {
     var rip = document.getElementsByClassName("aside");
     ripple(rip);
     function ripple(target) {
-        console.log(3);
+        console.log(4);
         var canvas = {},
             centerX = 0,
             centerY = 0,
@@ -24,6 +24,71 @@ $(function () {
             radius = 0,
 
         // 根据callback生成requestAnimationFrame动画,requestAnimationFrame 请求动画帧原理setTimeout
+        //         requestAnimFrame = function () {
+        //             return (
+        //                 window.requestAnimationFrame    ||
+        //                 window.mozRequestAnimationFrame  ||
+        //                 window.oRequestAnimationFrame   ||
+        //                 window.msRequestAnimationFrame   ||
+        //                 function (callback) {
+        //                     window.setTimeout(callback, 1000 / 60);
+        //                 }
+        //             );
+        //         } (),
+        //         // 为每个指定元素生成canves
+        //         init = function () {
+        //             containers = Array.prototype.slice.call(containers);
+        //             for (var i = 0; i < containers.length; i += 1) {
+        //                 canvas = document.createElement('canvas');
+        //                 canvas.addEventListener('click', press, false);
+        //                 containers[i].appendChild(canvas);
+        //                 canvas.style.width ='100%';
+        //                 canvas.style.height='100%';
+        //                 canvas.style.opacity='0.16';
+        //                 canvas.width = canvas.offsetWidth;
+        //                 canvas.height = canvas.offsetHeight;
+        //             }
+        //         },
+        //         // 点击并且获取需要的数据，如点击坐标、元素大小、颜色
+        //         press = function (event) {
+        //             // color = colorS();
+        //             element = event.toElement;
+        //             context = element.getContext('2d');
+        //             radius = 0;
+        //             //鼠标点击位置
+        //             centerX = event.offsetX;
+        //             centerY = event.offsetY;
+        //             context.clearRect(0, 0, element.width, element.height);//清空指定矩形框的像素，(x,y)起始点坐标
+        //             draw();
+        //         },
+        //         // 绘制圆形，并且执行动画
+        //         draw = function () {
+        //             context.beginPath();
+        //             context.arc(centerX, centerY, radius, 0, 2* Math.PI, false);
+        //             console.log(111);
+        //             context.fillStyle = colorS();
+        //             console.log(context.fillStyle);
+        //             context.fill();
+        //             radius += 2;
+        //             // 通过判断半径小于元素宽度，不断绘制 radius += 2 的圆形
+        //             if (radius < 40) {
+        //                 requestAnimFrame(draw);
+        //             }
+        //         };
+        //     //随机取颜色值rgb色值是十进制，10--16的进制转换
+        //     colorS = function(){
+        //         var s = "#";
+        //         function num(star,end){//随机取整
+        //             var n = Math.random()*end+star;
+        //             return parseInt(n);
+        //         }
+        //         s+=num(0,255).toString(16);
+        //         s+=num(0,255).toString(16);
+        //         s+=num(0,255).toString(16);
+        //         return s;
+        //     };
+        //     init();
+        // }
         requestAnimFrame = function () {
             return window.requestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
                 window.setTimeout(callback, 1000 / 60);
@@ -39,7 +104,7 @@ $(function () {
                 containers[i].appendChild(canvas);
                 canvas.style.width = '100%';
                 canvas.style.height = '100%';
-                canvas.style.opacity = '0.16';
+                canvas.style.opacity = '0.2';
                 canvas.width = canvas.offsetWidth;
                 canvas.height = canvas.offsetHeight;
             }
@@ -47,7 +112,8 @@ $(function () {
 
         // 点击并且获取需要的数据，如点击坐标、元素大小、颜色
         press = function press(event) {
-            // color = colorS();
+            //            color = colorS();
+            //            color = event.toElement.parentElement.dataset.color;
             element = event.toElement;
             context = element.getContext('2d');
             radius = 0;
@@ -61,28 +127,29 @@ $(function () {
         // 绘制圆形，并且执行动画
         draw = function draw() {
             context.beginPath();
-            context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-            console.log(111);
+            context.arc(centerX, centerY, radius, 0, 100 * Math.PI, false);
             context.fillStyle = colorS();
-            console.log(context.fillStyle);
             context.fill();
             radius += 2;
             // 通过判断半径小于元素宽度，不断绘制 radius += 2 的圆形
-            if (radius < 40) {
+            if (radius < element.width) {
                 requestAnimFrame(draw);
             }
         };
         //随机取颜色值rgb色值是十进制，10--16的进制转换
         colorS = function colorS() {
             var s = "#";
+
             function num(star, end) {
                 //随机取整
                 var n = Math.random() * end + star;
                 return parseInt(n);
             }
+
             s += num(0, 255).toString(16);
             s += num(0, 255).toString(16);
             s += num(0, 255).toString(16);
+            console.log(s);
             return s;
         };
         init();
