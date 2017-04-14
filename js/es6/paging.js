efeed3dbcdc70837a7daa850dfbfe0da778857bf
tantan0813 <1418346401@qq.com>
2017-04-14 15:36:54 +0800

    test

--- js/es6/paging.js
+++ js/es6/paging.js
@@ -11,7 +11,7 @@ $(function() {
     var rip=document.getElementsByClassName("aside");
     ripple(rip);
     function ripple(target){
-        console.log(31);
+        console.log(32);
         var canvas = {},
             centerX = 0,
             centerY = 0,
