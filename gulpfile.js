e4412879f83d07cc85c546b515798f51e26b02c7
tantan0813 <1418346401@qq.com>
2017-04-25 10:18:07 +0800

    gulp commit

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,9 @@ var gulp = require('gulp'),
     ftp = require('gulp-ftp'),
     gutil = require('gulp-util'),
     spritesmith = require('gulp.spritesmith'),//图片精灵
-    argv = require('minimist'),
-    zip = require('gulp-zip');//打包压缩
+    argv = require('minimist')(process.argv.slice(2)),
+    zip = require('gulp-zip'),//打包压缩
+    gp_deploy = require('gulp-gh-pages');//部署到git pages
 const autoprefixer = require('gulp-autoprefixer'),//自动添加浏览器前缀
         livereload = require('gulp-livereload');//自动刷新
 //JavaScript处理
@@ -37,28 +38,12 @@ gulp.task('Less', function () {
         .pipe(gulp.dest('css/css'))//将会在css下生成index.css
         .pipe(livereload());
 });
-
-//git提交
-gulp.task('commit', function(){
-    return gulp.src('../tantan0813.github.io/*')
-        .pipe(git.add())
-        .pipe(git.commit("test"));
-});
-//检查之前提交git版本
-gulp.task('checkout',["commit"], function () {
-    gitTag = argv.tag||defaultValue;
-    git.checkout(gitTag, function (err) {
-        if (err) throw err;
-    });
-});
-
 //第三方插件复制(swiper.js/jquery.js and so on)暂时无用
 gulp.task('copy', function (done) {
     return gulp.src("src/core/*")
         .pipe(gulp.dest("src/s"));
 });
-
-//运用gulp一键生成合成图片
+//运用gulp一键生成合成图片(图片精灵)
 gulp.task('sprite', function(){
     return gulp.src('img/*.png' )
         .pipe( spritesmith({
@@ -74,9 +59,30 @@ gulp.task('clean',function(){
 //打包
 gulp.task('zip_new', function () {
     var timeStamp = moment().format("YYYY-MM-D_HH-mm-ss_");
-   gulp.src("../tantan0813.github.io/*")
+    gulp.src(["../tantan0813.github.io/*/*/*","../tantan0813.github.io/*","../tantan0813.github.io/*/*"])
         .pipe(zip("dist_" + timeStamp + ".zip"))
         .pipe(gulp.dest("../tantan0813.github.io/zip"));
+});
+
+//==============wait handel=============
+//git pages branch git push
+var options = {};
+gulp.task('deploy', function () {
+    return gulp.src('../tantan0813.github.io/*')
+        .pipe(gp_deploy(options));
+});
+//git提交
+gulp.task('commit', function(){
+    return gulp.src(["../tantan0813.github.io/*/*/*","../tantan0813.github.io/*","../tantan0813.github.io/*/*"])
+        .pipe(git.add())
+        .pipe(git.commit());
+});
+//检查之前提交git版本
+gulp.task('checkout',["commit"], function () {
+    gitTag = argv.tag;
+    git.checkout(gitTag, function (err) {
+        if (err) throw err;
+    });
 });
 //发送到服务器
 gulp.task('ftp', function () {
@@ -92,20 +98,21 @@ gulp.task('ftp', function () {
     // .pipe( gulp.dest() );
 
 });
+//==========end============
 //gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组)
 //gulp.dest(path[, options]) 处理完后文件生成路径
 
-//仅本地处理JavaScript、HTML、less
+//仅本地监听处理JavaScript、HTML、less//watch监听方法（改变时运行），run直接运行（自执行一次）
 gulp.task('Watch', function () {
     livereload.listen();
-    //watch监听方法（改变时运行），run直接运行（自执行一次）
     gulp.watch('css/less/*.less', ['Less']); //当所有less文件发生改变时，调用testLess任务
     gulp.watch('js/es6/*.js', ['ES6']); //当所有less文件发生改变时，调用任务
 });
 //一键打包发送服务器
 gulp.task('git-v', function () {
-    //runSequence 运行任务队列
-    runSequence('clean','checkout',[ 'ES6','Less',"sprite"],'zip_new',function(){
+    //runSequence 运行任务队列(括号内为同步任务，方括号为异步任务)
+    runSequence('clean',[ 'ES6','Less',"sprite"],'zip_new',function(){
+    // runSequence('clean',"checkout",[ 'ES6','Less',"sprite"],'zip_new',"ftp",function(){
         console.log("你已成功提交"+ moment().format("YYYY-MM-D_HH-mm-ss_") +"版本")
     });
 });
